4510055d473f3b9ec7d2ac2b63e829ea8e41e5c5
CrystalW11 <crystal.warmack@gmail.com>
2025-04-03 10:47:37 -0600

    updated readme.md for each exercise

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Guided_Project_Grades
+
+
+### Create arrays and foreach loops
+
+![alt text](image.png)
+
--- obj/Debug/net9.0/08-Guided_Project_Grades.AssemblyInfo.cs
+++ obj/Debug/net9.0/08-Guided_Project_Grades.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("08-Guided_Project_Grades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0d0758067ea48780728c6bdfc633ea06a695a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("08-Guided_Project_Grades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("08-Guided_Project_Grades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
